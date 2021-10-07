3fa508746e05382c5118ed08c10a0273372cfee6
Dogue <dogue@oddhelix.com>
2021-10-07 17:19:39 -0400

    refactor player_info and server_info. refactor and improve errors

--- src/player_info.rs
+++ src/player_info.rs
@@ -1,39 +1,33 @@
-use crate::q3_error::Q3Error;
+use crate::error::Q3Error;
 
-#[derive(Debug, Clone)]
-pub struct PlayerInfo {
-    name: String,
+#[derive(Debug)]
+pub struct PlayerInfo<'a> {
+    name: &'a str,
     score: i32,
     ping: i32,
 }
 
-impl PlayerInfo {
-    pub fn new(player: &str) -> Result<Self, Q3Error> {
+impl<'a> PlayerInfo<'a> {
+    pub fn new(player: &'a str) -> Result<Self, Q3Error> {
         let mut player = player.splitn(3, ' ');
 
-        let score = player.next().unwrap_or("0").parse().unwrap_or_default();
-        let ping = player.next().unwrap_or("0").parse().unwrap_or_default();
-        let name = String::from(player.next().unwrap_or("0"));
+        let score = player.next().unwrap_or_default().parse()?;
+        let ping = player.next().unwrap_or_default().parse()?;
+        let name = player.next().unwrap_or_default();
 
         Ok(Self { name, score, ping })
     }
 
-    /// Names are passed as-is, meaning color escape codes and double quotes are still present.
-    /// A method for stripping/interpreting these *may* be implemented in the future.
-    ///
-    /// [See here](https://www.computerhope.com/issues/ch000658.htm) for more information on Q3 color codes.
-    pub fn name(&self) -> &String {
+    pub fn name(&self) -> &'a str {
         &self.name
     }
 
-    /// Player frag count. Represented in-engine as "score".
-    pub fn score(&self) -> &i32 {
-        &self.score
+    pub fn score(&self) -> i32 {
+        self.score
     }
 
-    /// Player latency
-    pub fn ping(&self) -> &i32 {
-        &self.ping
+    pub fn ping(&self) -> i32 {
+        self.ping
     }
 }
 
@@ -41,23 +35,42 @@ impl PlayerInfo {
 mod test {
     use super::*;
 
-    const TEST_STR: &'static str = "11 194 \"killer\"";
+    const VALID_PLAYER: &'static str = "11 194 \"dogue\"";
+    const INVALID_SCORE: &'static str = "a 194 \"dogue\"";
+    const INVALID_PING: &'static str = "11 a \"dogue\"";
 
     #[test]
-    fn extract_score() {
-        let p = PlayerInfo::new(TEST_STR).unwrap();
-        assert_eq!(11, p.score);
+    fn parses_score() {
+        let p = PlayerInfo::new(VALID_PLAYER);
+        assert!(p.is_ok());
+        assert_eq!(11, p.unwrap().score());
     }
 
     #[test]
-    fn extract_ping() {
-        let p = PlayerInfo::new(TEST_STR).unwrap();
-        assert_eq!(194, p.ping);
+    fn parses_ping() {
+        let p = PlayerInfo::new(VALID_PLAYER);
+        assert!(p.is_ok());
+        assert_eq!(194, p.unwrap().ping());
     }
 
     #[test]
-    fn extract_name() {
-        let p = PlayerInfo::new(TEST_STR).unwrap();
-        assert_eq!("\"killer\"", p.name);
+    fn parses_name() {
+        let p = PlayerInfo::new(VALID_PLAYER);
+        assert!(p.is_ok());
+        assert_eq!("\"dogue\"", p.unwrap().name());
+    }
+
+    #[test]
+    fn parseint_error_on_invalid_score() {
+        let p = PlayerInfo::new(INVALID_SCORE);
+        assert!(p.is_err());
+        assert!(matches!(p.err(), Some(Q3Error::ParseError(_))));
+    }
+
+    #[test]
+    fn parseint_error_on_invalid_ping() {
+        let p = PlayerInfo::new(INVALID_PING);
+        assert!(p.is_err());
+        assert!(matches!(p.err(), Some(Q3Error::ParseError(_))))
     }
 }
--- src/server_info.rs
+++ src/server_info.rs
@@ -1,58 +1,101 @@
-use crate::{player_info::PlayerInfo, q3_error::Q3Error};
+use crate::error::Q3Error;
+use crate::player_info::PlayerInfo;
 
-use super::player_list::PlayerList;
 use std::collections::HashMap;
 
-#[derive(Debug, Clone)]
-pub struct ServerInfo {
-    vars: HashMap<String, String>,
-    players: PlayerList,
+pub struct ServerInfo<'a> {
+    vars: HashMap<&'a str, &'a str>,
+    players: Vec<PlayerInfo<'a>>,
 }
 
-impl ServerInfo {
-    pub fn new(raw: &str) -> Result<Self, Q3Error> {
-        let (server_raw, player_raw) = raw.split_once("\n").unwrap_or_default();
-        let players = PlayerList::new(player_raw)?;
+impl<'a> ServerInfo<'a> {
+    pub fn new(raw_info: &'a str) -> Result<ServerInfo<'a>, Q3Error> {
+        let (server_chunk, player_chunk) = raw_info.split_once('\n').unwrap_or_default();
+        let vars = Self::build_server_vars(server_chunk);
+        let players = Self::build_player_list(player_chunk)?;
 
-        let mut vars: HashMap<String, String> = HashMap::new();
-        let mut server_raw = server_raw.split('\\');
+        Ok(Self { vars, players })
+    }
 
-        // Remove empty first element
-        server_raw.next();
+    pub fn vars(&self) -> &HashMap<&'a str, &'a str> {
+        &self.vars
+    }
 
-        for _i in 0..server_raw.clone().count() {
-            if let Some(key) = server_raw.next() {
-                let value = server_raw.next().unwrap_or("");
-                vars.insert(String::from(key), String::from(value));
+    pub fn players(&self) -> &Vec<PlayerInfo> {
+        &self.players
+    }
+
+    fn build_server_vars(server_chunk: &'a str) -> HashMap<&'a str, &'a str> {
+        let mut vars: HashMap<&'a str, &'a str> = HashMap::new();
+
+        let mut server_chunk = server_chunk.split('\\');
+        server_chunk.next(); // Discard garbage first element
+
+        for _i in 0..server_chunk.clone().count() {
+            if let Some(key) = server_chunk.next() {
+                let value = server_chunk.next().unwrap_or_default();
+                vars.insert(key, value);
             } else {
                 break;
             }
         }
 
-        Ok(Self { vars, players })
+        vars
     }
 
-    /// Keys of the `HashMap` are the same as they appear in raw output from the server. See the (truncated) example below.
-    ///
-    /// ```plain
-    /// fraglimit: 0
-    /// g_waverespawns: 0
-    /// g_redwave: 15
-    /// g_stratTime: 5
-    /// timelimit: 20
-    /// capturelimit: 8
-    /// g_roundtime: 2
-    /// sv_hostname: ^7|^1RFA^7| ^2RisenFromAshes.us
-    /// sv_maxPing: 0
-    /// ```
-    pub fn vars(&self) -> &HashMap<String, String> {
-        &self.vars
+    fn build_player_list(player_chunk: &'a str) -> Result<Vec<PlayerInfo>, Q3Error> {
+        let mut player_list: Vec<PlayerInfo> = Vec::new();
+
+        for p in player_chunk.split('\n') {
+            // Check if the first character is a null UTF-8 byte.
+            // This signals the end of the player list
+            let c = p.chars().nth(0).unwrap();
+            if c != '\u{0}' {
+                player_list.push(PlayerInfo::new(p)?);
+            }
+        }
+
+        Ok(player_list)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const VALID_PLAYER_CHUNK: &'static str =
+        "11 194 \"dogue\"\n11 194 \"dogue\"\n11 194 \"dogue\"\n\u{0}";
+
+    const VALID_SERVER_CHUNK: &'static str = /*"����statusResponse\n*/"\\players_blue\\1 2 4 \\players_red\\3 5 6 \\score_blue\\1\\score_red\\0\\score_time\\0:58\\sv_maxclients\\22\\capturelimit\\10\\fraglimit\\10\\email\\http://contact.fpsclasico.de\\url\\https://forum.fpsclasico.de\\administrator\\adminless\\dmflags\\72\\version\\Q3 1.32b.v6 linux-i386 Sep 24 2021\\g_gametype\\3\\protocol\\68\\mapname\\pro-q3dm13\\sv_hostname\\\u{7}      ! ^^--UnFreeZe^/.^-fpsclasico^/.^-de ^/(^+Oct 07^/, ^+03:07^/)\\sv_floodProtect\\1\\x_enable\\0\\bot_minplayers\\3\\www\\http://unfreeze.fpsclasico.de\\gamename\\unfreeze\\g_needpass\\0\\g_delagHitscan\\1\\g_unlaggedVersion\\2.0";
+
+    #[test]
+    fn correct_number_of_players() {
+        let p = ServerInfo::build_player_list(VALID_PLAYER_CHUNK);
+        assert!(p.is_ok());
+
+        let p = p.unwrap();
+        assert_eq!(p.len(), 3);
     }
 
-    /// Returns a `Vec` of `PlayerInfo` objects
-    ///
-    /// See [PlayerInfo] for more info
-    pub fn players(&self) -> &Vec<PlayerInfo> {
-        &self.players.0
+    #[test]
+    fn parses_server_vars() {
+        let s = ServerInfo::build_server_vars(VALID_SERVER_CHUNK);
+        assert_eq!(s.get("sv_maxclients").unwrap(), &"22");
+        assert_eq!(s.get("fraglimit").unwrap(), &"10");
+    }
+
+    #[test]
+    fn returns_server_vars() {
+        let combined_chunk = format!("{}\n{}", VALID_SERVER_CHUNK, VALID_PLAYER_CHUNK);
+        let s = ServerInfo::new(&combined_chunk).unwrap();
+        assert_eq!(s.vars().get("sv_maxclients").unwrap(), &"22");
+    }
+
+    #[test]
+    fn returns_player_list() {
+        let combined_chunk = format!("{}\n{}", VALID_SERVER_CHUNK, VALID_PLAYER_CHUNK);
+        let s = ServerInfo::new(&combined_chunk).unwrap();
+        assert_eq!(s.players().len(), 3);
+        assert_eq!(s.players()[0].name(), "\"dogue\"");
     }
 }
